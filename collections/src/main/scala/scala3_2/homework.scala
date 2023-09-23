0a22a79eed5a1a233a7126321ada1d701e382cb3
fjee <1102Fjee>
2023-09-23 19:32:38 +0300

    Opaque Types, given

--- collections/src/main/scala/scala3_2/homework.scala
+++ collections/src/main/scala/scala3_2/homework.scala
@@ -1,28 +1,34 @@
 package scala3_2
-/*
+
 object homework1 {
   extension (x: String)
-    def + ?????
+  def ++(y: String): Int = x.toInt + y.toInt
 
     @main def part1Ex(): Unit ={
-      println("1" + "33")
+      println("1" ++ "33")
     }
 }
 
 object homework2 {
   enum CompletionArg {
-    ???
-    //см приведенную ссылку
+    case StringOpt(s: String)
+    case IntOpt(i: Int)
+    case FloatOpt(f: Float)
   }
 
   object CompletionArg {
-    given fromString: Conversion[String, CompletionArg] = ???
+    given fromString: Conversion[String, CompletionArg] = StringOpt(_)
 
-    given fromInt: Conversion[Int, CompletionArg] = ???
+    given fromInt: Conversion[Int, CompletionArg] = IntOpt(_)
 
-    given fromFloat: Conversion[Float, CompletionArg] = ???
+    given fromFloat: Conversion[Float, CompletionArg] = FloatOpt(_)
   }
   import CompletionArg.*
+
+  def complete(ca: CompletionArg) = ca match
+    case StrType(s) => s"String type: $s"
+    case IntType(i) => s"Int type: $i"
+    case FloatType(f) => s"Float type: = $f"
 
   @main def part2Ex(): Unit ={
     println(Completions.complete("String"))
@@ -36,13 +42,13 @@ object homework3 {
   opaque type Logarithm = Double
 
   object Logarithm{
-    //см приведенную ссылку
+    def apply(d: Double) = math.log(d)
   }
 
   extension (x: Logarithm)
-    def toDouble: ???
-    def + (y: Logarithm): ???
-    def * (y: Logarithm): ???
+    def toDouble = math.exp(x)
+    def +(y: Logarithm) = Logarithm(math.exp(x) + math.exp(y))
+    def *(y: Logarithm) = x + y
 
 
   @main def part3Ex(): Unit ={
@@ -54,4 +60,4 @@ object homework3 {
     val l4 = l + l2
 
   }
-}*/
+}
